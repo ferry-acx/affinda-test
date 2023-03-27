e5d0e6374932a9d91e6f17121d8c4a2d8df2c868
jasjamjos <jasjamjos@gmail.com>
2023-03-27 17:14:24 +0800

    test

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -37,9 +37,9 @@ apiRoute.use(upload.array("file"));
 
 apiRoute.post(
   async (req: any, res: any) => {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     const fileList = fs.readdirSync(outputFolderName);
     const firstFile = `${outputFolderName}/${fileList[0]}`
     const {type} = req.query
