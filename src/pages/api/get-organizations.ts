cf76ed6d0187fa08133205563f9fff30d191b000
jasjamjos <jasjamjos@gmail.com>
2023-03-27 19:51:12 +0800

    test

--- src/pages/api/get-organizations.ts
+++ src/pages/api/get-organizations.ts
@@ -4,11 +4,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getOrganizations } from '@/utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await NextCors(req, res, {
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
-    optionsSuccessStatus: 200,
-  });
+  // await NextCors(req, res, {
+  //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  //   origin: "*",
+  //   optionsSuccessStatus: 200,
+  // });
   const response = await getOrganizations();
   res.status(200).json(response);
 }
